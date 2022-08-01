6566af0bf8b91a09e1ba5d4bad961fbe98e8fabb
KyunKyuu <teguh.iqbal782@gmail.com>
2022-08-01 22:37:54 +0700

    safe from sql injection

--- sql_test.go
+++ sql_test.go
@@ -117,3 +117,33 @@ func TestSqlInjection(t *testing.T){
 	}
 
 }
+
+func TestSqlInjectionSafe(t *testing.T){
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "salah"
+
+	sql := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows,err := db.QueryContext(ctx, sql, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next(){
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+			fmt.Println("Login Berhasil")
+	}else {
+		fmt.Println("Login Gagal")
+	}
+
+}
