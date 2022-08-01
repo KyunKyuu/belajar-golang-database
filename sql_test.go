3117a2408552a370d8f4eb4e33b02473e4f6184d
KyunKyuu <teguh.iqbal782@gmail.com>
2022-08-01 20:36:06 +0700

    sql data column

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T){
@@ -44,6 +45,45 @@ func TestQuerySql(t *testing.T){
 
 		fmt.Println("ID : ", id)
 		fmt.Println("Name : ", name)
+	}
+
+}
+
+func TestQuerySqlComplex(t *testing.T){
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	sql := "SELECT id,name,email,balance,rating,birth_date,married,created_at FROM customer"
+	rows,err := db.QueryContext(ctx, sql)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next(){
+		var id, name, email string
+		var balance int32
+		var rating float64
+		var birthDate, createdAt time.Time
+		var married bool
+
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("ID : ", id)
+		fmt.Println("Name : ", name)
+		fmt.Println("Email : ", email)
+		fmt.Println("Balance : ", balance)
+		fmt.Println("Rating : ", rating)
+		fmt.Println("Birth Date : ", birthDate)
+		fmt.Println("Married : ", married)
+		fmt.Println("Created At : ", createdAt)
 	}
 
 }
